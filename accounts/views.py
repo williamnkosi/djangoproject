9bd89ad2d24cb5655e7e84fa28b6cc82989b5231
William Nkosi <william.dev.acc@gmail.com>
2020-07-19 12:21:35 -0500

    added messaging to django

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
-
+from django.contrib import messages
 # Create your views here.
 
 
 def register(request):
     if request.method == 'POST':
-        print('SUBMITTED REG')
-        return redirect('index')
+        messages.error(request, 'Testing error message')
+        return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
--- btre/settings.py
+++ btre/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+from django.contrib.messages import constants as messages
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -135,3 +136,10 @@ STATICFILES_DIRS = [
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+# Messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+
+}
