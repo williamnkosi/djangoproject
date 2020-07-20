5328fbb60db047fab047f89fe0c60d37396e64eb
William Nkosi <william.dev.acc@gmail.com>
2020-07-19 21:55:17 -0500

    added user login and logout funtionality

--- accounts/views.py
+++ accounts/views.py
@@ -44,14 +44,26 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-        print('SUBMITTED REG')
-        return redirect('index')
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid Credentials')
+            return redirect('login')
     else:
         return render(request, 'accounts/login.html')
 
 
 def logout(request):
-    return redirect('index')
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'You have been logged out')
+        return redirect('index')
 
 
 def dashboard(request):
